0fe3984ea01511e5f10ef97bbe5c6d90f7b39b23
Amej0016 <162392699+Amej0016@users.noreply.github.com>
2024-03-06 14:10:57 -0600

    Persona0

---
+++ Subclase/src/subclase/Subclase.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package subclase;
+
+/**
+ *
+ * @author Angel
+ */
+public class Subclase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+
+# Crear instancias de las clases y mostrar su información personal
+persona1 = Per.("Geraldine", 2)
+print(pers.1.obtener_informacion_pers.l())
+
+doctor1 = Doc.("Sayli", 25, "Medico General")
+print(doctor1.obtener_informacion_personal())
+
+deportista1 = Dep.("Angel", 25, "Fútbol")
+print(deportista1.obtener_informacion_personal())
+
